984401db9a75607636d6aeb713a334eeceb43643
Marc Teyssier <marc.teys@gmail.com>
2013-09-12 10:50:40 +0100

    new readme file

--- js/grid.js
+++ js/grid.js
@@ -10,40 +10,6 @@
 04/08/13 : 23h24 | Amélioration du rechargemlent des images
 
 
-A faire :
-
-  Enlever le loader quand les images sont chargés uniquement
-  Elevation du terrain/sprite a corriger ?
-  Plantage des arbres
-  Amélioration drag and drop
-  Exporter et importer
-  Changer le design
-  Ajout dynamique de row
-  si amélioration du rechargement : modification du design des sprites
-
-
-Sauvegarde d'un sketrch
-
-  o Sauver l'image sur le serveur //ajax+php (http://stackoverflow.com/questions/13198131/how-to-save-a-html5-canvas-as-image-on-a-server)
-  o La redimentionner  //php
-  o Définir un nom au dessin
-  o Exporter les coordonnée
-  o Schéma base de donnée : 
-      - ID | Utilisateur | Nom terrain | Date | radomname
-  o Ouvrir l'url avec /randomname
-  o Ouvrir l'url avec dsétail des tuiles
-
-Load d'un sketch :
-
-  o Entrée par nom d'utilisateur
-  o Entrée par date
-  o Entrée par nom de sketch
-  o Selectionner : Regarcher canvas
-
-
-Jouabiloité :
-
-  Création d'une voiture/objet qui se déplace
 
 
 
